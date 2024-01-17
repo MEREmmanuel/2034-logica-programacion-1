d0133a46a569004b01bff1e74b53522b30e80aae
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-01-17 17:44:07 -0600

    add challenges -> 01. Iniciando con Javascript

---
+++ Challenges/01.iniciando.con.javascript/solutions.js
@@ -0,0 +1,21 @@
+// statement 1
+alert('Welcome to our website!');
+// statement 2
+let name = "Luna";
+// statement 3
+let age = 25;
+// statement 4
+let numberofSales = 50;
+// statement 5
+let availableBalance = 1000;
+// statement 6
+alert('Error! Complete all fields');
+// statement 7
+let errorMessage = "Error! Complete all fields";
+alert(errorMessage);
+// statement 8
+name = prompt("Please, insert your name");
+// statement 9
+age = prompt("Please, insert your age");
+//statement 10
+if(age >= 18) alert('You can get your driver licence');
---
+++ app.js
@@ -0,0 +1,4 @@
+let secretNumber = 10;
+let insertedNumber = prompt("Please, insert a number.");
+console.log('Number saved: ', insertedNumber);
+if (insertedNumber == secretNumber) alert('Congratulations! You got the secret number');
