3c78b563e0e5fc38405b318d120a875e7018659d
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-01-17 18:20:56 -0600

    implement random secret number & else conditional

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let secretNumber = 10;
-let insertedNumber = prompt("Please, insert a number.");
-console.log('Number saved: ', insertedNumber);
-if (insertedNumber == secretNumber) alert('Congratulations! You got the secret number');
+let secretNumber = Math.floor(Math.random() * 10) + 1;
+let insertedNumber = prompt("Please, insert a number from 1 to 10.");
+
+if (insertedNumber == secretNumber) alert('Congratulations! You got the secret number.');
+else alert('Sorry, you did not get the secret number.');
