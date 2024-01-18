c0eb3ceaf24e0c18a93cf35d68b1a1895059ff53
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-01-17 18:54:02 -0600

    remove solutions.js, insert solutions in 01.iniciando.con.javascript.md & insert string interpolation into app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let secretNumber = Math.floor(Math.random() * 10) + 1;
 let insertedNumber = prompt("Please, insert a number from 1 to 10.");
 
-if (insertedNumber == secretNumber) alert('Congratulations! You got the secret number.');
+if (insertedNumber == secretNumber) alert(`Congratulations! You got the secret number, that is ${secretNumber}.`);
 else alert('Sorry, you did not get the secret number.');
