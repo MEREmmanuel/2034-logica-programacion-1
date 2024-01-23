47fb61b16256dc0ef104619fe923e89b3d834a3c
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-01-22 18:41:26 -0600

    implement break and ternary operator to project

--- Project/app.js
+++ Project/app.js
@@ -1,20 +1,26 @@
 let secretNumber = Math.floor(Math.random() * 10) + 1;
 let insertedNumber = 0;
 let attempts = 1;
-let wordAttempts = 'atemmpt';
+let maximunAttempts = 2;
 
 while (insertedNumber != secretNumber) {
   insertedNumber = prompt("Please, insert a number from 1 to 10.");
   console.log(secretNumber, insertedNumber);
   if (insertedNumber == secretNumber)
     alert(
-      `Congratulations! You got the secret number (${secretNumber}), in ${attempts} ${wordAttempts}.`
+      `Congratulations! You got the secret number (${secretNumber}), in ${attempts} ${
+        attempts == 1 ? "attempt" : "attempts"
+      }.`
     );
   else {
     if (insertedNumber > secretNumber)
       alert("Sorry, the secret number is less.");
     else alert("Sorry, the secret number is greater.");
-    attempts = attempts + 1;
-    wordAttempts = 'attempts';
+    attempts++;
+    wordAttempts = "attempts";
+    if (attempts > maximunAttempts) {
+      alert(`You reached the maximum number of ${maximunAttempts} attempts`);
+      break;
+    }
   }
 }
