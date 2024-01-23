3d185c73cd0474f708c82461b10870d78a1c469f
MEREmmanuel <informatica_21887037@cintalapa.tecnm.mx>
2024-01-22 18:22:46 -0600

    add challenges -> 03. Loops y tentativas

--- Project/app.js
+++ Project/app.js
@@ -1,5 +1,20 @@
 let secretNumber = Math.floor(Math.random() * 10) + 1;
-let insertedNumber = prompt("Please, insert a number from 1 to 10.");
+let insertedNumber = 0;
+let attempts = 1;
+let wordAttempts = 'atemmpt';
 
-if (insertedNumber == secretNumber) alert(`Congratulations! You got the secret number, that is ${secretNumber}.`);
-else alert('Sorry, you did not get the secret number.');
+while (insertedNumber != secretNumber) {
+  insertedNumber = prompt("Please, insert a number from 1 to 10.");
+  console.log(secretNumber, insertedNumber);
+  if (insertedNumber == secretNumber)
+    alert(
+      `Congratulations! You got the secret number (${secretNumber}), in ${attempts} ${wordAttempts}.`
+    );
+  else {
+    if (insertedNumber > secretNumber)
+      alert("Sorry, the secret number is less.");
+    else alert("Sorry, the secret number is greater.");
+    attempts = attempts + 1;
+    wordAttempts = 'attempts';
+  }
+}
